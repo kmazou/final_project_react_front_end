f2d079c970006aacf5799e59a70ad9deed747bee
Karim Ali <admin@mikeanalytics.com>
2024-12-19 02:51:31 +0900

    Build

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [], // Initialize items as an empty array.
   },
   reducers: {
     addItem: (state, action) => {
